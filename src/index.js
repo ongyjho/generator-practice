7b00bbd8d3272e045c4256ef28a9259dd034c11f
Yeojin Hong <58408421+ongyjho@users.noreply.github.com>
2020-10-01 13:24:56 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function* makeNumber() {
     yield num++;
   }
 }
-//기존에는 함수의 값을 리턴하는 것이다. yieldㄴ
+//기존에는 함수의 값을 리턴하는 것이다. yield
 
 const y = makeNumber(); //실제로 실행되지는 않고, 실행할 수 있는 메소드가 담긴 객체
 console.log(y.next()); //yield나 return 이 나올때까지 yield는 retrun 과 달리 함수를 끝내지 않는다. 값을 내보내기만 할뿐이다.
